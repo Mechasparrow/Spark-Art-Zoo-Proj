1398f7b3475914168711bf5966acf8c03c17fa39
Mechasparrow <mikipux7@gmail.com>
2018-09-19 21:57:00 -0500

    small beginning changes

--- explori/src/App.js
+++ explori/src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Explori</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="Items">
+          <h2 className="title">Exhibits</h2>
+          <ul className="list">
+            <li>American Art</li>
+            <li>Contemporary Art</li>
+            <li>European Art</li>
+            <li>Asian Art</li>
+          </ul>
+        </div>
       </div>
     );
   }
