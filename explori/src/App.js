6caab5d9d9d935931a51592653d563d252b9c344
Mechasparrow <mechasparrow@gmail.com>
2018-09-20 13:56:07 -0500

    exhibit item

--- explori/src/App.js
+++ explori/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 // Additional components
 import HeaderBar from "./Components/HeaderBar";
+import ExhibitItem from "./Components/ExhibitItem";
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
 
         <div className="Items">
           <h2 className="title">Exhibits</h2>
+          <ExhibitItem></ExhibitItem>
           <ul className="list">
             <li>American Art</li>
             <li>Contemporary Art</li>
--- explori/src/Components/ExhibitItem.js
+++ explori/src/Components/ExhibitItem.js
@@ -1,4 +1,51 @@
 import React, {Component} from 'react';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-class Exhibit
+//Material elements
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 245,
+    marginLeft: '20%',
+    marginRight: '20%'
+  },
+  title: {
+    marginBottom: 16,
+    padding: '8px'
+  }
+}
+
+class ExhibitItem extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {classes} = this.props;
+
+    return (
+      <div className = "ExhibitItem">
+        <Card className = {classes.card}>
+          <Typography variant = "headline" component = "h2" className = {classes.title}>
+            Hello world
+          </Typography>
+        </Card>
+      </div>
+    )
+  }
+
+}
+
+ExhibitItem.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ExhibitItem);
