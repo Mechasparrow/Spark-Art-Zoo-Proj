4c13ccad3d09759482729a2c33b8f3ed9b50c950
Mechasparrow <mechasparrow@gmail.com>
2018-10-10 12:04:17 -0500

    Commented scraper models

--- scraper/models/Collection.py
+++ scraper/models/Collection.py
@@ -1,11 +1,20 @@
+#
+# ./models/Collection.py
+# Collection Model
+#
+# A collection has a name and items
+#
+
 import json
 
 class Collection:
 
+    # collection constructor
     def __init__(self, name, items):
         self.name = name
         self.items = items
 
+    # convert model to dictionary
     def __dict__(self):
 
         item_dicts = []
@@ -17,10 +26,11 @@ class Collection:
             "items": item_dicts
         }
 
-
+    # return json version of object
     def __json__(self):
         json_str = json.dumps(self.__dict__())
         return json_str
 
+    # string version of the Collection model
     def __str__(self):
         return self.__json__()
--- scraper/models/Item.py
+++ scraper/models/Item.py
@@ -1,7 +1,15 @@
+#
+# ./models/Item.py
+# Item Model
+#
+# A item has a title, description, author, location, image_link
+#
+
 import json
 
 class Item:
 
+    # item constructor
     def __init__(self, title, description, author, location, image_link, type=None):
         self.title = title
         self.author = author
@@ -10,6 +18,7 @@ class Item:
         self.image_link = image_link
         self.type = type
 
+    # convert model to dictionary
     def __dict__(self):
 
         return {
@@ -21,9 +30,11 @@ class Item:
             "type": self.type
         }
 
+    # return json version of object
     def __str__(self):
         dict = self.__dict__()
         return json.dumps(dict)
 
+    # string version of Item model
     def __json__(self):
         return self.__str__()
