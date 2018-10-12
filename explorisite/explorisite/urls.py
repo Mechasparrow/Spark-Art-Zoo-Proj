bb0638572d742ccce8b7d9ddf7dabd907b515ed2
Mechasparrow <mikipux7@gmail.com>
2018-10-11 22:22:24 -0500

    poor attempt at docs

--- explorisite/explorisite/urls.py
+++ explorisite/explorisite/urls.py
@@ -16,12 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework.documentation import include_docs_urls
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('backend.urls')),
-    path('docs/', include_docs_urls(title='Explori Api',
-                                        authentication_classes=[],
-                                        permission_classes=[]))
+    path('api/', include('backend.urls'))
 ]
