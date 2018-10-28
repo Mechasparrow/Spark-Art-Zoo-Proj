cd7414177766d3a69f60d0c632adbbf7bddf01ed
Mechasparrow <mikipux7@gmail.com>
2018-10-28 01:01:29 -0500

    added rudimentary social media sharing

--- explori/src/Components/BadgeCard.js
+++ explori/src/Components/BadgeCard.js
@@ -20,6 +20,18 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 
+//material icons
+import Icon from "@material-ui/core/Icon";
+import IconButton from "@material-ui/core/IconButton";
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+//Font awesome icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+//Social Sharing
+import { FacebookButton, TwitterButton } from "react-social";
+
 //Routing
 import { Link, Redirect } from "react-router-dom";
 
@@ -33,8 +45,8 @@ const styles = {
     minWidth: 245
   },
   title: {
-    marginBottom: 16,
-    padding: "8px"
+    padding: "8px",
+    paddingBottom: "0px"
   },
   card_image_container: {
     textAlign: "center"
@@ -50,6 +62,47 @@ const styles = {
     opacity: 0.25
   },
   actions: {}
+};
+
+//sharing buttons
+let FBButton = props => {
+  let { classes } = props;
+
+  return (
+    <IconButton size="large" onClick={props.onClick}>
+      <FontAwesomeIcon icon={faFacebook} />
+    </IconButton>
+  );
+};
+
+let disabledFBButton = props => {
+  let { classes } = props;
+
+  return (
+    <IconButton disabled size="large" onClick={props.onClick}>
+      <FontAwesomeIcon icon={faFacebook} />
+    </IconButton>
+  );
+};
+
+let TweetButton = props => {
+  let { classes } = props;
+
+  return (
+    <IconButton size="large" onClick={props.onClick}>
+      <FontAwesomeIcon icon={faTwitter} />
+    </IconButton>
+  );
+};
+
+let disabledTweetButton = props => {
+  let { classes } = props;
+
+  return (
+    <IconButton disabled size="large" onClick={props.onClick}>
+      <FontAwesomeIcon icon={faTwitter} />
+    </IconButton>
+  );
 };
 
 //declare BadgeCard class
@@ -75,6 +128,12 @@ class BadgeCard extends Component {
       completed_class = classes.uncomplete;
     }
 
+    //set a share message
+    let share_message =
+      "Hey, I just got the " +
+      this.props.collection.name +
+      " badge from Explori";
+
     return (
       <div className="BadgeCard">
         <Card className={classes.card}>
@@ -87,7 +146,41 @@ class BadgeCard extends Component {
               >
                 {this.props.collection.name}
               </Typography>
+              <div className={classes.share_buttons}>
+                {this.props.completed && (
+                  <FacebookButton
+                    url={"http://example.com/"}
+                    appId={"553013885135102"}
+                    element={FBButton}
+                    message={share_message}
+                  />
+                )}
 
+                {!this.props.completed && (
+                  <FacebookButton
+                    url={""}
+                    appId={"553013885135102"}
+                    element={disabledFBButton}
+                    message={share_message}
+                  />
+                )}
+
+                {this.props.completed && (
+                  <TwitterButton
+                    url={"http://example.com/"}
+                    element={TweetButton}
+                    message={share_message}
+                  />
+                )}
+
+                {!this.props.completed && (
+                  <TwitterButton
+                    url={""}
+                    element={disabledTweetButton}
+                    message={share_message}
+                  />
+                )}
+              </div>
               <div className={classes.card_image_container}>
                 <img
                   src={this.props.badge.image_link}
